0cd48e3be1f192ce2fe37a436db7285f4f84d718
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-05-02 20:55:31 +0600

    Updated chef details API

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const chefData = require("./data/chef.json");
 const app = express();
 
-
 app.get("/", (req, res) => {
-    res.send("Chef website server")
+  res.send("Chef website server");
 });
 
 app.get("/chef", (req, res) => {
-    res.send(chefData)
-})
+  res.send(chefData);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chefDetails = chefData.find((chef) => chef.id == id);
+  res.send(chefDetails);
+});
+
 app.listen(5000, () => {
-    console.log("Server is running on port 5000...");
+  console.log("Server is running on port 5000...");
 });
