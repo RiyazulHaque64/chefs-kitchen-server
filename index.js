16e5ceafd05fe2c1087cce46311b3eaed7564380
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-05-04 03:10:04 +0600

    Add blog data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const chefData = require("./data/chef.json");
+const blogs = require("./data/blogs.json");
 const cors = require("cors");
+
 const app = express();
 
 app.use(cors());
@@ -17,6 +19,10 @@ app.get("/chef/:id", (req, res) => {
   const id = req.params.id;
   const chefDetails = chefData.find((chef) => chef.id == id);
   res.send(chefDetails);
+});
+
+app.get("/blogs", (req, res) => {
+  res.send(blogs);
 });
 
 app.listen(5000, () => {
